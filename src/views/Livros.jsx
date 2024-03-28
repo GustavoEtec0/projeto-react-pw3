a1a40c81981019a6274a08c8c7815cb97d53176a
GustavoEtec0 <160188621+GustavoEtec0@users.noreply.github.com>
2024-03-28 20:39:42 -0300

    Initial commit

---
+++ src/views/Livros.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Livros() {
+    return <div>Livros</div>
+}
---
+++ src/views/NovoLivro.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function NovoLivro() {
+    return <div>NovoLivro</div>
+}
