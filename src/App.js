7d8c1d20199103479fc9bd4f90258d1d9faa09a1
GustavoEtec0 <160188621+GustavoEtec0@users.noreply.github.com>
2024-04-04 22:02:54 -0300

    select

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ import Navbar from './components/Navbar'
 import Container from './components/Container'
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Container>
-                    <Routes>
-                        <Route path="/" element={<Navbar />}>
-                            <Route index element={<Home />} />
-                            <Route path="/livros" element={<Livros />} />
-                            <Route path="/novo-livro" element={<NovoLivro />} />
-                        </Route>
-                    </Routes>
-                </Container>
-            </BrowserRouter>
-        </div>
-    )
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Navbar />}>
+              <Route index element={<Home />} />
+              <Route path="/livros" element={<Livros />} />
+              <Route path="/novo-livro" element={<NovoLivro />} />
+            </Route>
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
