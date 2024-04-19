c15fc8e9c41c522b96561643507cd683ca6994a6
GustavoEtec0 <160188621+GustavoEtec0@users.noreply.github.com>
2024-04-18 22:22:57 -0300

    Add files via upload

--- src/views/Livro/index.jsx
+++ src/views/Livro/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './style.css'
+import Message from '../../components/Message'
+import { useLocation } from 'react-router-dom'
 
 export default function Livros() {
+    const location = useLocation()
+    let message = ''
+
+    if (location.state) {
+        message = location.state
+    }
     return (
         <section className="livro_container">
             <div>
                 <h3>livros</h3>
+                {message && <Message msg={message} type="error" />}
             </div>
         </section>
     )
